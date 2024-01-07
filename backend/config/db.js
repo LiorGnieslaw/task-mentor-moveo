d7b988c6dd1cb5587eb60bc97c5c2a9320086a80
Lior Gnieslaw <lior.gnieslaw@post.runi.ac.il>
2024-01-07 19:37:34 +0200

    fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,7 @@ const uri = "mongodb+srv://liorgni4:Lg123456@cluster0.wjhic2b.mongodb.net/sample
 const connectDB = async () => {
   try {
     console.log('trying yo connect mongo')
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(uri);
     console.log('MongoDB Connected');
   } catch (error) {
     console.log('fuckkk')
